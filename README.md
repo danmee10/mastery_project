54da94bc70449a6989713aafb786e522e20e9bd1
Dan Mee <danmee10@gmail.com>
2013-07-04 15:09:18 -0600

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-mastery_project
-===============
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasteryProject::Application.config.secret_token = '43b6cfbd84e802d92bf4da646a11897440ddf17715749e02044d597411aee264a05053046e79b8688401120fa9fb958c5493f62bd6b3056ad48427c492bfb5fd'
